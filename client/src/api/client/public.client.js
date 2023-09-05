9fcd99198cb1a7c0a47168df498ff6776649cb44
Roman Kalistratov <80212286+roman-kalistratov@users.noreply.github.com>
2023-09-05 22:34:31 +0300

    client url init

--- client/src/api/client/public.client.js
+++ client/src/api/client/public.client.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 import queryString from "query-string";
 
-
-const baseURL = "http://localhost:5000/api/v1/";
+// const baseURL = "http://localhost:5000/api/v1/";
+const baseURL = "http://authorization-back.vercel.app/api/v1/";
 
 const publicClient = axios.create({
   baseURL,
